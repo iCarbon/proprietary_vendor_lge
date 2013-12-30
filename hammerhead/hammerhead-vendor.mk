c27846cfaa1cb0c9e3636777922101297dd0de98
Brint E. Kriebel <github@bekit.net>
2013-12-29 16:25:46 -0800

    hammerhead: Add APKs as PRODUCT_PACKAGES
    This allows for re-signing with release keys
    
    Change-Id: I650f4d1fbb00a7f563ee6820b6fda932dcaf014f

--- hammerhead/hammerhead-vendor.mk
+++ hammerhead/hammerhead-vendor.mk
@@ -26,4 +26,16 @@ PRODUCT_PACKAGES := \
 PRODUCT_COPY_FILES := \
     packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:/system/etc/permissions/android.software.live_wallpaper.xml
 
+# Pick up overlay for features that depend on non-open-source files
+DEVICE_PACKAGE_OVERLAYS := vendor/lge/hammerhead/overlay
+
+# Apps
+PRODUCT_PACKAGES += \
+    OmaDmclient \
+    qcrilmsgtunnel \
+    SprintHiddenMenu \
+    shutdownlistener \
+    TimeService \
+    UpdateSetting
+
 $(call inherit-product, vendor/lge/hammerhead/hammerhead-vendor-blobs.mk)
